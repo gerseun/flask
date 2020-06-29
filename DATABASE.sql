d3271c92fac6a74340e1a234b452192827229e3b
Tecnico8 <Tecnico8@DOMINIO.DOGICAN>
2020-06-29 14:44:06 +0200

    DB2

--- DATABASE.sql
+++ DATABASE.sql
@@ -30,11 +30,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `articolo` (
   `id_art` int(11) NOT NULL,
-  `cod_art` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `desc_art` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `cli_art` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `cod_cli_art` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `kit_art` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `cod_art` varchar(50) NOT NULL,
+  `desc_art` varchar(50) DEFAULT NULL,
+  `cli_art` varchar(50) DEFAULT NULL,
+  `cod_cli_art` varchar(50) DEFAULT NULL,
+  `kit_art` varchar(50) DEFAULT NULL,
   `data_art` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -46,8 +46,8 @@ CREATE TABLE `articolo` (
 
 CREATE TABLE `articolo_componenti` (
   `id_artcomp` int(11) NOT NULL,
-  `id_art` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `id_comp` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id_art` varchar(50) NOT NULL,
+  `id_comp` varchar(50) NOT NULL,
   `qt_comp` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -64,30 +64,11 @@ CREATE TABLE `backup_riga_dett` (
   `id_comp_b` int(11) NOT NULL,
   `qt_comp_b` int(11) NOT NULL,
   `id_produzione_b` int(11) NOT NULL,
-  `pos_comp_imp_b` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `pos_comp_imp_b` varchar(50) NOT NULL,
   `data_b` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `cod_ordine_b` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cod_ordine_b` varchar(50) DEFAULT NULL,
   `scadenza_b` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `backup_riga_imp_comp`
---
-
-CREATE TABLE `backup_riga_imp_comp` (
-  `id_backup_comp` int(11) NOT NULL,
-  `id_riga_imp_comp_b` int(11) NOT NULL,
-  `id_imp_b` int(11) NOT NULL,
-  `id_comp_b` int(11) NOT NULL,
-  `qt_comp_b` int(11) NOT NULL,
-  `data_cons_comp_b` timestamp NOT NULL,
-  `id_produzione_b` int(11) NOT NULL,
-  `pos_comp_sing_imp_b` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `cod_ordine_b` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `scadenza_b` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -97,12 +78,12 @@ CREATE TABLE `backup_riga_imp_comp` (
 
 CREATE TABLE `componente` (
   `id_comp` int(11) NOT NULL,
-  `cod_comp` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `grezzo` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `desc_comp` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `dim_comp` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `mat_comp` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `pos_comp` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `cod_comp` varchar(50) NOT NULL,
+  `grezzo` varchar(50) DEFAULT NULL,
+  `desc_comp` varchar(50) DEFAULT NULL,
+  `dim_comp` varchar(50) DEFAULT NULL,
+  `mat_comp` varchar(50) DEFAULT NULL,
+  `pos_comp` varchar(50) DEFAULT NULL,
   `data_comp` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -114,9 +95,9 @@ CREATE TABLE `componente` (
 
 CREATE TABLE `impegno` (
   `id_imp` int(11) NOT NULL,
-  `cod_imp` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `cliente` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `cod_ord_cli` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `cod_imp` varchar(50) NOT NULL,
+  `cliente` varchar(50) DEFAULT NULL,
+  `cod_ord_cli` varchar(50) DEFAULT NULL,
   `data_ord` timestamp NULL DEFAULT NULL,
   `data_comp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -133,8 +114,8 @@ CREATE TABLE `riga_dett` (
   `id_comp` int(11) NOT NULL,
   `qt_comp` int(11) NOT NULL,
   `id_produzione` int(11) NOT NULL,
-  `pos_comp_imp` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0',
-  `cod_ordine` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `pos_comp_imp` varchar(50) NOT NULL DEFAULT '0',
+  `cod_ordine` varchar(50) DEFAULT NULL,
   `scadenza` timestamp NULL DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -150,24 +131,6 @@ CREATE TABLE `riga_imp` (
   `id_art` int(11) NOT NULL,
   `qt_art` int(11) NOT NULL,
   `data_cons_art` timestamp NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `riga_imp_comp_AFK`
---
-
-CREATE TABLE `riga_imp_comp_AFK` (
-  `id_riga_imp_comp` int(11) NOT NULL,
-  `id_imp` int(11) NOT NULL,
-  `id_comp` int(11) NOT NULL,
-  `qt_comp` int(11) NOT NULL,
-  `data_cons_comp` timestamp NOT NULL,
-  `id_produzione` int(11) NOT NULL,
-  `pos_comp_sing_imp` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '0',
-  `cod_ordine` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `scadenza` timestamp NULL DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -196,13 +159,6 @@ ALTER TABLE `backup_riga_dett`
   ADD UNIQUE KEY `id_riga_dett_b` (`id_riga_dett_b`,`id_produzione_b`);
 
 --
--- Indici per le tabelle `backup_riga_imp_comp`
---
-ALTER TABLE `backup_riga_imp_comp`
-  ADD PRIMARY KEY (`id_backup_comp`),
-  ADD UNIQUE KEY `id_riga_imp_comp_b` (`id_riga_imp_comp_b`,`id_produzione_b`);
-
---
 -- Indici per le tabelle `componente`
 --
 ALTER TABLE `componente`
@@ -229,11 +185,6 @@ ALTER TABLE `riga_dett`
 ALTER TABLE `riga_imp`
   ADD PRIMARY KEY (`id_riga_imp`);
 
---
--- Indici per le tabelle `riga_imp_comp_AFK`
---
-ALTER TABLE `riga_imp_comp_AFK`
-  ADD PRIMARY KEY (`id_riga_imp_comp`);
 
 --
 -- AUTO_INCREMENT per le tabelle scaricate
@@ -258,12 +209,6 @@ ALTER TABLE `backup_riga_dett`
   MODIFY `id_backup` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT per la tabella `backup_riga_imp_comp`
---
-ALTER TABLE `backup_riga_imp_comp`
-  MODIFY `id_backup_comp` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT per la tabella `componente`
 --
 ALTER TABLE `componente`
@@ -286,13 +231,6 @@ ALTER TABLE `riga_dett`
 --
 ALTER TABLE `riga_imp`
   MODIFY `id_riga_imp` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la tabella `riga_imp_comp_AFK`
---
-ALTER TABLE `riga_imp_comp_AFK`
-  MODIFY `id_riga_imp_comp` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
